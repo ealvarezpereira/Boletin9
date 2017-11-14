04aee7dd4f7903d8a6b0a9f323bdcfebf6703da5
ealvarezpereira@danielcastelao.org <>
2017-11-14 08:50:34 +0100

    B9ej3: Clase

---
+++ B9ej3/src/b9ej3/B9ej3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b9ej3;
+
+/**
+ *
+ * @author quique
+ */
+public class B9ej3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Area newarea = new Area();
+        newarea.calcularArea();
+    }
+    
+}
