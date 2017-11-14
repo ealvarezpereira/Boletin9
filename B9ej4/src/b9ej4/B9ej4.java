35e91bd5dd95fed991892f64d947614da85edc99
ealvarezpereira@danielcastelao.org <quique@LAPTOP-2PRGJ9F7>
2017-11-14 09:38:16 +0100

    B9ej4

---
+++ B9ej4/src/b9ej4/B9ej4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b9ej4;
+
+/**
+ *
+ * @author quique
+ */
+public class B9ej4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TablaMultiplicar tabla1 = new TablaMultiplicar();       
+        tabla1.calcularTabla();
+    }
+    
+}
