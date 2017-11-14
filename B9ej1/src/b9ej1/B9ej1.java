2666d2935bd8729749b6f579d2f26b65ff110403
ealvarezpereira@danielcastelao.org <>
2017-11-14 08:48:14 +0100

    B9ej1

---
+++ B9ej1/src/b9ej1/B9ej1.java
@@ -0,0 +1,17 @@
+package b9ej1;
+
+/**
+ *
+ * @author quique
+ */
+public class B9ej1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Contador count = new Contador();
+        count.contadorNum();
+    }
+    
+}
