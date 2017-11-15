502537d0a48b04def53879c51ba78842696189bd
ealvarezpereira@danielcastelao.org <>
2017-11-15 10:16:18 +0100

    B9ej6

---
+++ B9ej6/src/b9ej6/B9ej6.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b9ej6;
+
+/**
+ *
+ * @author quique
+ */
+public class B9ej6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Sueldos trab = new Sueldos();
+        trab.calcularSueldo();
+    }
+    
+}
